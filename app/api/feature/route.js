ad6589ea042f10720c3fed7cd87e778f7eb15b07
abdullahirfansatti-commits <abdullahirfansatti@gmail.com>
2026-02-20 04:59:25 -0500

    Remove unused POST function from route.js
    Removed commented-out POST function for user insertion.

--- app/api/feature/route.js
+++ app/api/feature/route.js
@@ -7,14 +7,3 @@ export async function GET(){
   const users = await db.collection("feature").find({}).toArray();
   return NextResponse.json(users); 
 }
-
-// export async function POST(req) {
-//   const body = await req.json();
-
-//   const client = await clientPromise;
-//   const db = client.db("mydatabase");
-
-//   const result = await db.collection("users").insertOne(body);
-
-//   return NextResponse.json(result);
-// }
