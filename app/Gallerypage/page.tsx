f2a09cb474ccb004f9dc40992b390a5c5e226040
abdullahirfansatti-commits <abdullahirfansatti@gmail.com>
2026-02-21 16:13:43 -0500

    Updated purifier project

--- app/Gallerypage/page.tsx
+++ app/Gallerypage/page.tsx
@@ -1,5 +1,11 @@
+"use client"
+import { useEffect } from "react";
 import Gallery from "../components/Gallery";
 export default function Gallerypage(){
+
+   useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify([{ id: 1 }, {id: 2}]));
+  }, []);
     return(
       <>
            <Gallery/>
